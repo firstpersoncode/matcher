03455c075544d9b29d7a56bc7aae44287270bd9b
firstpersoncode <fpcdatadriver@gmail.com>
2023-03-08 13:59:17 +0700

    adjust match card styling

--- src/components/MatchCard.jsx
+++ src/components/MatchCard.jsx
@@ -69,10 +69,12 @@ export default function MatchCard({match, onPress}) {
                   fontWeight: 'bold',
                   marginBottom: 8,
                 }}
-                variant="titleLarge">
+                variant={isParticipant ? 'labelLarge' : 'titleLarge'}>
                 {match.name}
               </Text>
-              <Text variant="titleMedium">{match.provider.name}</Text>
+              <Text variant={isParticipant ? 'labelSmall' : 'titleMedium'}>
+                {match.provider.name}
+              </Text>
               {!isParticipant && (
                 <Text variant="bodyMedium">{match.provider.address}</Text>
               )}
@@ -83,61 +85,60 @@ export default function MatchCard({match, onPress}) {
                 <Button
                   mode="contained"
                   icon="map-marker"
-                  style={{width: 150, marginBottom: 8}}
+                  style={{marginBottom: 8}}
                   onPress={toggleVisibleMap}>
                   ~ {Math.floor(match.distance / 1000)} km
                 </Button>
               )}
 
-              <Button
-                style={{width: 150}}
-                mode="contained-tonal"
-                icon="account">
+              <Button mode="contained-tonal" icon="account">
                 {joinedCount} / {match.count}
               </Button>
             </View>
           </View>
         </Card.Content>
       </TouchableRipple>
-      {visibleMap && (
-        <Map
-          title={match.provider.name}
-          description={match.provider.address}
-          center={match.location.coordinates.slice().reverse()}
-          height={300}
-          liteMode={false}
-        />
-      )}
-      <Divider />
       {!isParticipant && (
-        <TouchableRipple onPress={onPress}>
-          <Card.Content style={{padding: 16}}>
-            <View
-              style={{
-                padding: 4,
-                borderRadius: 10,
-                backgroundColor: theme.colors.secondaryContainer,
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}>
-              <IconButton
-                icon="calendar-check"
-                size={15}
-                style={{padding: 0}}
-              />
-              <Text
+        <>
+          {visibleMap && (
+            <Map
+              title={match.provider.name}
+              description={match.provider.address}
+              center={match.location.coordinates.slice().reverse()}
+              height={300}
+              liteMode={false}
+            />
+          )}
+          <Divider />
+          <TouchableRipple onPress={onPress}>
+            <Card.Content style={{padding: 16}}>
+              <View
                 style={{
-                  flex: 1,
-                  color: theme.colors.onPrimaryContainer,
-                  fontWeight: 'bold',
+                  padding: 4,
+                  borderRadius: 10,
+                  backgroundColor: theme.colors.secondaryContainer,
+                  flexDirection: 'row',
+                  alignItems: 'center',
                 }}>
-                {format(new Date(match.start), 'iiii | do MMM yyyy |')}{' '}
-                {format(new Date(match.start), 'HH:mm')} -{' '}
-                {format(new Date(match.end), 'HH:mm')}
-              </Text>
-            </View>
-          </Card.Content>
-        </TouchableRipple>
+                <IconButton
+                  icon="calendar-check"
+                  size={15}
+                  style={{padding: 0}}
+                />
+                <Text
+                  style={{
+                    flex: 1,
+                    color: theme.colors.onPrimaryContainer,
+                    fontWeight: 'bold',
+                  }}>
+                  {format(new Date(match.start), 'iiii | do MMM yyyy |')}{' '}
+                  {format(new Date(match.start), 'HH:mm')} -{' '}
+                  {format(new Date(match.end), 'HH:mm')}
+                </Text>
+              </View>
+            </Card.Content>
+          </TouchableRipple>
+        </>
       )}
     </Card>
   );
